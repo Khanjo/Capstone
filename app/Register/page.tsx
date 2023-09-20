c03005ece9962aa2cfe71c0436d9527bf8a2e1a3
Joshua Khan <Khanjo@gmail.com>
2023-09-20 11:48:40 -0700

    work on registration page

--- app/Register/page.tsx
+++ app/Register/page.tsx
@@ -1,17 +1,15 @@
-import styles from '../page.module.css'
+import styles from './page.module.css'
 
 export default function Login() {
     return (
         <>
-            <main>
-                <h1>Register</h1>
+            <h1>Register</h1>
 
-                <form>
-                    <input type="text" name="email" placeholder='email' />
-                    <input type='text' name='password' placeholder='password' />
-                    <button type='submit'>Register</button>
-                </form>
-            </main>
+            <form className={styles.register}>
+                <input type="text" name="email" placeholder='email' className={styles.inputs} /><br />
+                <input type='text' name='password' placeholder='password' className={styles.inputs} /><br />
+                <button type='submit' className={styles.button}>Register</button>
+            </form>
         </>
     )
 }
