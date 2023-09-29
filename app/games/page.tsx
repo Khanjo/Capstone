55ab3d862f4029817d1c437fc93df8f90ff7e5f3
Joshua Khan <Khanjo@gmail.com>
2023-09-29 09:13:54 -0700

    add games page and game component

---
+++ app/games/page.tsx
@@ -0,0 +1,14 @@
+import styles from './page.module.css'
+import Link from 'next/link'
+
+export default function Games() {
+    <>
+        <h1>Games</h1>
+
+        <ul>
+
+        </ul>
+
+        <button onClick={addGame}>Add Game</button>
+    </>
+}
