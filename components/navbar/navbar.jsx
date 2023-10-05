cfd1c692f5fdcc1944f8d928c7e164ea493b51ea
Joshua Khan <Khanjo@gmail.com>
2023-10-05 10:17:28 -0700

    update navbar

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import styles from './navbar.module.css';
 import { signOut } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Navbar() {
     return (
@@ -9,6 +10,12 @@ export default function Navbar() {
             <Link href="register"><h2>Register</h2></Link>
             <Link href="login"><h2>Login</h2></Link>
             <button className={styles.signOut} onClick={() => signOut()}>Log Out</button>
+            <Image
+                src='/HungryGuy.gif'
+                alt='HungryGuy'
+                width={640}
+                height={64}
+                className={styles.hungry} />
             <div className={styles.searchBar}>
                 <input className={styles.searchInput} type='text' />
                 <button className={styles.searchButton}>&#128269;</button>
